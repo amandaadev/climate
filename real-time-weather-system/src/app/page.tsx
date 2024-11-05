45533a7411597feb6f58767ab1b7e7354689a113
Amanda Hellen <amanda.hellen@g.globo>
2024-11-05 10:14:34 -0300

    feat: update changes

--- real-time-weather-system/src/app/page.tsx
+++ real-time-weather-system/src/app/page.tsx
@@ -2,7 +2,47 @@
 
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { WeatherData } from "./types";
+import { AlertaPersonalizadoProps, WeatherData } from "./types";
+
+const AlertaPersonalizado: React.FC<AlertaPersonalizadoProps> = ({
+  mensagem,
+  onClose,
+}) => {
+  return (
+    <div
+      className="fixed top-4 right-4 bg-red-600 text-white p-4 rounded-md flex items-center shadow-lg"
+      role="alert"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6 mr-2"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M13 16h-1v-4h-1m1 8h.01M12 12V8m0 4h.01m-6.93 5.42a2 2 0 00-.516-.134 2 2 0 00-2.196.921c-.294.43-.858.67-1.404.622-.526-.047-1.053-.374-1.337-.918-.367-.684-.486-1.48-.324-2.215C.924 11.136 3.8 4.05 12 4c8.2.05 11.076 7.136 11.686 9.98.161.736.043 1.53-.324 2.215-.284.544-.811.871-1.337.918-.546.048-1.11-.191-1.404-.622a2 2 0 00-2.196-.921c-.184.053-.355.134-.516.134h-.1"
+        />
+      </svg>
+      <span>{mensagem}</span>
+      <button
+        className="ml-4 text-sm text-white hover:underline"
+        onClick={onClose}
+      >
+        Fechar
+      </button>
+    </div>
+  );
+};
+
+const Spinner = () => (
+  <div className="flex justify-center items-center">
+    <div className="animate-spin h-8 w-8 border-4 border-t-4 border-fuchsia-400 border-t-transparent rounded-full"></div>
+  </div>
+);
 
 const WeatherPage = () => {
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
@@ -56,19 +96,12 @@ const WeatherPage = () => {
     }
   };
 
-  if (loading)
-    return <p className="text-center text-2xl font-bold">Carregando...</p>;
-  if (error)
-    return (
-      <p className="text-red-600 text-center font-bold text-2xl">{error}</p>
-    );
-
   return (
     <>
-      <h1 className="py-7 px-4 sm:py-10 sm:px-9 text-left text-[40px] sm:text-[60px] font-bold text-fuchsia-200 font-poppins  ">
+      <h1 className="py-7 px-4 sm:py-10 sm:px-9 text-left text-[40px] sm:text-[60px] font-bold text-fuchsia-200 font-poppins">
         Real Time Weather System
       </h1>
-      <div className="text-center mb-4 text-black ">
+      <div className="text-center mb-4 text-black">
         <input
           type="text"
           value={inputValue}
@@ -84,35 +117,44 @@ const WeatherPage = () => {
           Buscar
         </button>
       </div>
-      <div className="mx-auto ml-4 mr-4  sm:mx-auto sm:p-10 p-4 max-w-md bg-[#7873dc] min-h-[200px] rounded-3xl">
-        <h2 className="sm:text-[30px]/[40px] text-[25px] font-bold text-center font-poppins">
-          Clima em {weatherData?.name}
-        </h2>
-        {weatherData && weatherData.weather && weatherData.weather[0] && (
-          <Image
-            src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
-            alt={weatherData.weather[0].description}
-            width={50}
-            height={500}
-            className="mx-auto"
-          />
+      {error && (
+        <AlertaPersonalizado mensagem={error} onClose={() => setError(null)} />
+      )}
+      <div className="mx-auto ml-4 mr-4 sm:mx-auto sm:p-10 p-4 max-w-md bg-[#7873dc] h-200px min-h-[200px] rounded-3xl">
+        {loading ? (
+          <Spinner />
+        ) : (
+          <>
+            <h2 className="sm:text-[30px]/[40px] text-[25px] font-bold text-center font-poppins">
+              Clima em {weatherData?.name}
+            </h2>
+            {weatherData && weatherData.weather && weatherData.weather[0] && (
+              <Image
+                src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+                alt={weatherData.weather[0].description}
+                width={50}
+                height={500}
+                className="mx-auto"
+              />
+            )}
+            <p className="mt-2 text-center font-poppins">
+              Temperatura:{" "}
+              {weatherData?.main.temp !== undefined
+                ? Math.round(weatherData.main.temp) + " °C"
+                : "N/A"}
+            </p>
+
+            <p className="text-center font-poppins">
+              Descrição: {weatherData?.weather[0].description}
+            </p>
+            <p className="text-center font-poppins">
+              Umidade: {weatherData?.main.humidity}%
+            </p>
+            <p className="text-center font-poppins">
+              Vento: {weatherData?.wind.speed} m/s
+            </p>
+          </>
         )}
-        <p className="mt-2 text-center font-poppins ">
-          Temperatura:{" "}
-          {weatherData?.main.temp !== undefined
-            ? Math.round(weatherData.main.temp)
-            : "N/A"}
-          °C
-        </p>
-        <p className="text-center font-poppins ">
-          Descrição: {weatherData?.weather[0].description}
-        </p>
-        <p className="text-center font-poppins ">
-          Umidade: {weatherData?.main.humidity}%
-        </p>
-        <p className="text-center font-poppins ">
-          Vento: {weatherData?.wind.speed} m/s
-        </p>
       </div>
     </>
   );
--- real-time-weather-system/src/app/types.ts
+++ real-time-weather-system/src/app/types.ts
@@ -4,3 +4,8 @@ export interface WeatherData {
   main: { temp: number; humidity: number };
   wind: { speed: number };
 }
+
+export interface AlertaPersonalizadoProps {
+  mensagem: string;
+  onClose: () => void;
+}
